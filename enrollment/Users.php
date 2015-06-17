f0c15c86c9202c844b1c120456ca62bdda781070
DarkDrium <sendoarata@gmail.com>
2015-06-17 20:38:36 +0800

    update erwin

---
+++ enrollment/Users.php
@@ -0,0 +1,43 @@
+<div id="left_nav" style="width:29%">
+					<div data-role="collapsible-set">
+						<div data-role="collapsible">
+						  	  <h4>Students</h4>
+							    <ul data-role="listview">
+							        <li><a href="#"  class='function' id='assessment' >Assessment</a></li>
+							        <li><a href="#"  class='function' id='enrollment'>Enrollment</a></li>
+							   
+							    </ul>
+						</div>
+						<div data-role="collapsible">
+						  	  <h4>Teacher</h4>
+							    <ul data-role="listview">
+							        <li><a href="#"  class='function' id='load'>Load</a></li>
+							        <li><a href="#"  class='function' id='profile'>Profile</a></li>
+							    </ul>
+						</div>
+					</div>
+				</div>
+
+				<div id="body" style="width:69%;">
+						<div data-role='content' id='container'>
+								<div class='assessment'>
+									
+									Assessmen
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse molestias fugit, corrupti possimus aliquid? Obcaecati facere, possimus eaque nemo. Reiciendis libero dignissimos est repellendus neque cumque, eligendi, delectus et natus!
+								</div>
+								<div class='enrollment'>
+									
+									enrollment
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, animi sit iusto odio aperiam nam. Repellat, consequatur, soluta tempore eligendi earum odio explicabo reiciendis ratione ea fugiat mollitia unde perspiciatis.
+								</div>
+								<div class='load'>
+									load
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate debitis, eaque maxime, corporis quibusdam eos nobis inventore, sit eius animi, porro placeat ea praesentium illo amet reiciendis. Non, fugiat, illum.
+								</div>
+								<div class='profile'>
+									profile
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam eius deserunt ad rerum impedit dignissimos ipsa blanditiis harum excepturi, omnis ducimus ipsum architecto quae consequuntur iure inventore facere, tenetur voluptates.
+								</div>
+						</div>
+				</div>
+	
--- enrollment/js/script.js
+++ enrollment/js/script.js
@@ -12,12 +12,30 @@ $( function() {
 	console.log("panel: " + panel);
 	console.log("panelheight: " + panelheight);
 
+
 	$('#container').children().hide();
 
-	$('.function').click(function(){
-			id = $(this).attr('id');
-	$('#container').children().hide();
-		$("#container div[class='"+id+"']").fadeIn();
+
+
+	$('.menu_cl').click(function(){
+			var value = $(this).text();
+			
+			$('.contents').empty();
+			$('.contents').load(value+".php",function(){
+			$('.contents').fadeOut(0);
+				$('.contents').fadeIn('1000');
+				$('.contents').trigger('create');
+				
+			$('.function').bind('click',function(){
+							id = $(this).attr('id');
+			$('#container').children().hide();
+				$("#container div[class='"+id+"']").fadeIn();
+
+			});
+			});
+
+
 	});
 
-} );
+	
+});
